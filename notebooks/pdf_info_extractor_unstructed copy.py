b8774ca359ceac8a0291180736dcf93bd9fb52c3
Hudanyun Sheng <hudanyun.sheng@zennioptical.com>
2024-07-31 08:47:42 +0800

    update notebook

--- notebooks/pdf_info_extractor_unstructed copy.py
+++ notebooks/pdf_info_extractor_unstructed copy.py
@@ -1,3 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: jpfaq
+#     language: python
+#     name: python3
+# ---
+
 # %%
 import os
 from unstructured.partition.pdf import partition_pdf
@@ -8,22 +24,27 @@ current_working_directory = os.getcwd()
 # Print the current working directory
 print(current_working_directory)
 
-# %%
-from unstructured_inference.models.tables import cells_to_html
+# %% [markdown]
+# ```
+# conda install conda-forge::poppler  
+# conda install conda-forge::tesseract  
+# ```
+
+# %% [markdown]
+# On Mac:  
+# `brew install tesseract`  
+# `brew install poppler-utils`
 
 # %%
-path_pdf = "data/JP Label/stelara iv Japanese PI.pdf"
+# from unstructured_inference.models.tables import cells_to_html
+
+# %%
+path_pdf = "../data/JP Label/stelara iv Japanese PI.pdf"
 # path_pdf = "/Workspace/Users/dsheng3@its.jnj.com/github/JP_FAQ/data/JP Label/stelara sc Japanese PI.pdf"
 filename = path_pdf.split("/")[-1].split(".pdf")[0]
-dir_partitions = "output/JP Label images partition/"
+dir_partitions = "../output/JP Label images partition/"
 if not os.path.exists(dir_partitions):
     os.makedirs(dir_partitions)
-
-# %%
-import nltk
-
-# %%
-nltk.download('punkt')
 
 # %%
 raw_pdf_elements = partition_pdf(
@@ -32,7 +53,7 @@ raw_pdf_elements = partition_pdf(
     extract_images_in_pdf=True,                            # mandatory to set as ``True``
     extract_image_block_types=["Image", "Table"],          # optional
     extract_image_block_to_payload=False,                  # optional
-    # extract_image_block_output_dir=path + "images/" + file_name.split(".")[0] + "/",  # optional - only works when 
+    extract_image_block_output_dir=dir_partitions + filename, #.split(".")[0] + "/",  # optional - only works when 
     chunking_strategy="by_title",
     # extract_image_block_to_payload=True
     multipage_sections = "False",
@@ -41,58 +62,18 @@ raw_pdf_elements = partition_pdf(
 
 # %%
 print(f"In total {len(raw_pdf_elements)} elements")
-print(f"Categories of all elements are: {set([ele.category for ele in raw_pdf_elements])}")
+ele_types = set([ele.category for ele in raw_pdf_elements])
+print(f"Categories of all elements are: {ele_types}")
 
 # %%
-tables = []
+ele_groups = dict((key, []) for key in ele_types)
+
 for ele in raw_pdf_elements:
-    if ele.category == "Table":
-        tables.append(ele)
-print(f"{len(tables)} tables")
+    ele_groups[ele.category].append(ele)
+print(f"{len(ele_groups['Table'])} tables")
 
 # %%
-print(tables[-1].metadata.text_as_html)
-
-# %%
-dir(tables[0].metadata)
-
-# %%
-tables[0].metadata.page_number
-
-# %%
-tables[0].metadata.fields
-
-# %% [markdown]
-# COMMAND ----------
-
-# %%
-dir(tables[0].metadata)
-
-# %% [markdown]
-# COMMAND ----------
-
-# %%
-dir(tables[0])
-
-# %% [markdown]
-# COMMAND ----------
-
-# %%
-dir(tables[0])
-
-# %% [markdown]
-# COMMAND ----------
-
-# %%
-tables[0].to_dict()
-
-# %% [markdown]
-# COMMAND ----------
-
-# %%
-print("\n\n".join([str(el) for el in raw_pdf_elements]))
-
-# %% [markdown]
-# COMMAND ----------
+txt_tables = [x.to_dict() for x in ele_groups['Table']]
+txt_images = [x.to_dict() for x in ele_groups['CompositeElement']]
 
 # %%
